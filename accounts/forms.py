9fe246ae044a441064915a29b0729fe8d84ed8be
sreebash <sreebashdas.1@gmail.com>
2022-07-02 16:42:21 +0600

    add custom registration form validation error message

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,18 +6,52 @@ from django.forms import TextInput
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, help_text='First Name',
+    first_name = forms.CharField(max_length=30, required=False, help_text='First Name',
+                                 error_messages={'required': 'First Name is Required'},
                                  widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=30, required=True, help_text='Last Name',
+    last_name = forms.CharField(max_length=30, required=False, help_text='Last Name',
                                 widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
     username = forms.CharField(max_length=50, required=True, help_text='Username',
                                widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    email = forms.EmailField(max_length=255, help_text='Email', widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    email = forms.EmailField(max_length=255, help_text='Email',
+                             widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     password1 = forms.Field(help_text='Password',
-        widget=forms.PasswordInput(attrs={'class': "form-control", 'placeholder': 'Password'}))
+                            widget=forms.PasswordInput(attrs={'class': "form-control", 'placeholder': 'Password'}))
     password2 = forms.Field(help_text='Retype Password',
-        widget=forms.PasswordInput(attrs={'class': "form-control", 'placeholder': 'Confirm Password'}))
+                            widget=forms.PasswordInput(
+                                attrs={'class': "form-control", 'placeholder': 'Confirm Password'}))
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2',)
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == '':
+            raise forms.ValidationError('First name is required')
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if last_name == '':
+            raise forms.ValidationError('Last name is required')
+        return last_name
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username is not unique")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is not unique")
+        return email
+
+    def clean_confirm_password(self):
+        confirm_password = self.cleaned_data.get('confirm_password')
+        original_password = self.cleaned_data.get('password', '')
+        if original_password != confirm_password:
+            raise forms.ValidationError("Password didn't match!")
+        return confirm_password
--- accounts/views.py
+++ accounts/views.py
@@ -14,23 +14,6 @@ def home(request):
     return render(request, 'home.html')
 
 
-#
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             first_name = form.cleaned_data['Email']
-#
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-#             return redirect('home')
-#     form = SignUpForm()
-#     return render(request, 'registration/signup.html', {'form': form})
-
-
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
